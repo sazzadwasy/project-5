fd57115a7beb899a304497fbdf659f8c7e8bd395
sazzadwasy <sazzad.wasy@gmail.com>
2022-02-18 11:34:59 +0600

    function commit

--- js/money.js
+++ js/money.js
@@ -1,26 +1,51 @@
+const income = document.getElementById('income')
+const food = document.getElementById('food-cost')
+const rent = document.getElementById('rent-cost')
+const clothes = document.getElementById('clothes-cost')
+
 function updateBalance() {
-    const income = document.getElementById('income').value
-    const food = document.getElementById('food-cost').value
-    const rent = document.getElementById('rent-cost').value
-    const clothes = document.getElementById('clothes-cost').value
-    const totalCost = parseFloat(food) + parseFloat(rent) + parseFloat(clothes)
+
+    const totalCost = parseInt(food.value) + parseInt(rent.value) + parseInt(clothes.value)
     document.getElementById('expences').innerText = totalCost
-    const balanceRemain = parseFloat(income) - totalCost
+    const balanceRemain = parseInt(income.value) - totalCost
     document.getElementById('balance-remain').innerText = balanceRemain
 }
-
 const calculate = document.getElementById('calculate').addEventListener('click', function () {
+    // validation 
+    if (
+        isNaN(parseInt(income.value)) ||
+        isNaN(parseInt(food.value)) ||
+        isNaN(parseInt(rent.value)) ||
+        isNaN(parseInt(clothes.value)) ||
+        parseInt(income.value) < 0 ||
+        parseInt(food.value) < 0 ||
+        parseInt(rent.value) < 0 ||
+        parseInt(clothes.value) < 0
+    ) {
+        return alert("Please input a valid number");
+    }
     updateBalance()
 
 })
 const saveMoney = document.getElementById('save-button').addEventListener('click', function () {
-    const income = document.getElementById('income').value
-    const saveInput = document.getElementById('save-field').value
-    const savingAmount = (parseFloat(saveInput) / 100) * parseFloat(income)
-    document.getElementById('saving').innerText = savingAmount
-    const LastBalanceText = document.getElementById('balance-remain').innerText
-    const lastBalance = parseFloat(LastBalanceText)
-    const totalRemain = lastBalance - savingAmount
-    document.getElementById('total-remain').innerText = totalRemain
-
+    const income = getInputValue('income')
+    const saveInput = getInputValue('save-field')
+    if (saveInput < 0 || isNaN(saveInput)) {
+        return alert("Please input a valid number")
+    } else {
+        const savingAmount = (parseInt(saveInput) / 100) * parseInt(income)
+        document.getElementById('saving').innerText = savingAmount
+        const LastBalanceText = document.getElementById('balance-remain').innerText
+        const lastBalance = parseInt(LastBalanceText)
+        if (savingAmount > lastBalance) {
+            return alert("You can't save more then your remaining balance ")
+        }
+        const totalRemain = lastBalance - savingAmount
+        document.getElementById('total-remain').innerText = totalRemain
+    }
 })
+
+function getInputValue(id) {
+    const inputValue = document.getElementById(id)
+    return parseInt(inputValue.value)
+}
